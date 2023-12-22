5dd0416d09c3803aac5f7782a6638a68448d37e0
Jamison Rubino <jamison.rubino@gmail.com>
2023-12-22 13:14:23 -0800

    seo meta tags

--- jr/src/Components/services.js
+++ jr/src/Components/services.js
@@ -32,63 +32,46 @@ export default function Services() {
     console.log("Services");
     return (
         <div>
-            <h1 className="my-3">Services</h1>
-            <div className="row mx-4">
-                <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div className="card bg-success text-light">
+            {/* <h1 className="mt-3 mb-4 pb-2">Services</h1> */}
+            <div className="row mx-4 mt-4 pt-3">
+                <div className="col-sm-6 col-md-4 col-lg-3 mb-4 pb-3">
+                    <div className="card bg-light text-dark shadow">
                         <div className="card-body pb-4">
                             <h4 className="card-title pb-1">Moving Help</h4>
                             <p className="card-text">Professional moving help with floor dollies, blankets, and moving straps to get your belongings from A to B safely.</p>
                         </div>
                         <ul className="list-group list-group-flush">
-                                <li className="list-group-item bg-success text-light"><p className="h4 pt-3">$55/hr</p><p className="h6 pt-0">$45/hr <font style={{fontWeight: 300}}>after first hour</font></p></li>
-                                <li className="list-group-item bg-success text-light">        <Link to="../Contact" className="btn btn-secondary"  state={{service: "Moving Help"}}>Get a Quote</Link>
+                                <li className="list-group-item bg-light text-dark"><p className="h4 pt-3">$55/hr</p><p className="h6 pt-0">$45/hr <font style={{fontWeight: 300}}>after first hour</font></p></li>
+                                <li className="list-group-item bg-light text-dark">        <Link to="../Contact" className="btn btn-secondary"  state={{service: "Moving Help"}}>Get a Quote</Link>
                                 </li>
-                                <li className="list-group-item bg-success text-light pb-3">        
+                                <li className="list-group-item bg-light text-success pb-3">        
                                 <Link to="https://jamison-rubino.bookafy.com/category/moving-help-1803b3?locale=en" className="btn btn-dark"  state={{service: "Moving Help"}} target="_blank">Book Now</Link>
                                 </li>
                         </ul>
                     </div>
                 </div>
-                <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div className="card bg-success text-light">
+                <div className="col-sm-6 col-md-4 col-lg-3 mb-4 pb-3">
+                    <div className="card bg-light text-dark shadow">
                         <div className="card-body pb-4">
                             <h4 className="card-title pb-1">Furniture Assembly</h4>
                             <p className="card-text">Professional assembly and disassembly with a comprehensive tool kit to help you perfect your home or office.</p>
                         </div>
                         <ul className="list-group list-group-flush">
-                                <li className="list-group-item bg-success text-light"><p className="h4 pt-3">$60/hr</p><p className="h6 pt-0">$50/hr <font style={{fontWeight: 300}}>after first hour</font></p></li>
-                                <li className="list-group-item bg-success text-light">        
+                                <li className="list-group-item bg-light text-dark"><p className="h4 pt-3">$60/hr</p><p className="h6 pt-0">$50/hr <font style={{fontWeight: 300}}>after first hour</font></p></li>
+                                <li className="list-group-item bg-light text-success">        
                                 <Link to="../Contact" className="btn btn-secondary" state={{service: "Furniture Assembly"}}>Get a Quote</Link>
                                 </li>
-                                <li className="list-group-item bg-success text-light pb-3">        
+                                <li className="list-group-item bg-light text-success pb-3">        
                                 <Link to="https://jamison-rubino.bookafy.com/category/furniture-assembly-8d3332?locale=en" className="btn btn-dark"  state={{service: "Moving Help"}} target="_blank">Book Now</Link>
                                 </li>
 
                         </ul>
                     </div>
                 </div>
-                <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div className="card bg-success text-light">
-                        <div className="card-body pb-4">
-                            <h4 className="card-title pb-1">Electrical Installation <strong>(New)</strong></h4>
-                            <p className="card-text">Light fixture and electrical outlet installations. <strong>First 20 bookings discounted 20%</strong>.</p>
-                        </div>
-                        <ul className="list-group list-group-flush">
-                                <li className="list-group-item bg-success text-light"><p className="h4 pt-3"><s className="text-dark">$52/hr</s> <strong>$42/hr</strong></p><p className="h6 pt-0"><s className="text-dark">$42/hr</s> <strong>$34/hr</strong> <font style={{fontWeight: 300}}>after first hour</font></p></li>
-                                <li className="list-group-item bg-success text-light">        
-                                <Link to="../Contact" className="btn btn-secondary"  state={{service: "Electrical Installation"}}>Get a Quote</Link>
-                                </li>
-                                <li className="list-group-item bg-success text-light pb-3">        
-                                <Link to="https://jamison-rubino.bookafy.com/category/moving-help-1803b3?locale=en" className="btn btn-dark"  state={{service: "Moving Help"}} target="_blank">Book Now</Link>
-                                </li>
-                        </ul>
-                    </div>
-                </div>
 
 
-                <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div className="card bg-dark text-light">
+                <div className="col-sm-6 col-md-4 col-lg-3 mb-4 pb-3">
+                    <div className="card bg-dark text-light shadow">
                         {/* <img className="card-img-top" src="..." alt="Card image cap" /> */}
                         <div className="card-body pb-4">
                             <h4 className="card-title pb-1">Web Development</h4>
@@ -105,8 +88,8 @@ export default function Services() {
                         </ul>
                     </div>
                 </div>
-                <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
-                    <div className="card bg-dark text-light">
+                <div className="col-sm-6 col-md-4 col-lg-3 mb-4 pb-3">
+                    <div className="card bg-dark text-light shadow">
                         <div className="card-body pb-4">
                             {/* onClick={()=>{setSelectedService('Search Engine Optimization'); handleShow() }}*/}
                             <h4 className="card-title pb-1">Search Engine Optimization</h4>
@@ -123,7 +106,27 @@ export default function Services() {
                         </ul>
                     </div>
                 </div>
-                <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div className="col-sm-6 col-md-4 col-lg-3 mb-4 pb-3">
+                    <div className="card text-dark">
+                        <div className="card-body pb-4">
+                            <h4 className="card-title pb-1">Electrical Installation</h4>
+                            {/* <p className="card-text">Light fixture and electrical outlet installations. <strong>First 20 bookings discounted 20%</strong>.</p> */}
+                            <p className="card-text">Coming soon!</p>
+                            <Link to="../Contact" className="btn btn-primary disabled"  state={{service: "Electrical Installation"}}>Get a Quote</Link>
+                        </div>
+                        {/* <ul className="list-group list-group-flush">
+                                <li className="list-group-item bg-light text-success"><p className="h4 pt-3"><s className="text-dark">$52/hr</s> <strong>$42/hr</strong></p><p className="h6 pt-0"><s className="text-dark">$42/hr</s> <strong>$34/hr</strong> <font style={{fontWeight: 300}}>after first hour</font></p></li>
+                                <li className="list-group-item bg-light text-success">        
+                                <Link to="../Contact" className="btn btn-secondary disabled"  state={{service: "Electrical Installation"}}>Get a Quote</Link>
+                                </li>
+                                <li className="list-group-item bg-light text-success pb-3">        
+                                <Link to="https://jamison-rubino.bookafy.com/category/moving-help-1803b3?locale=en" className="btn btn-dark disabled"  state={{service: "Moving Help"}} target="_blank">Book Now</Link>
+                                </li>
+                        </ul> */}
+                    </div>
+                </div>
+
+                <div className="col-sm-6 col-md-4 col-lg-3 mb-4 pb-3">
                     <div className="card">
                         <div className="card-body pb-4">
                             <h4 className="card-title pb-1">Plumbing</h4>
