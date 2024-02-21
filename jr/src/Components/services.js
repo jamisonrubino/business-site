888af7e9cf076163c275cffe48a51f0781c2d9c1
Jamison Rubino <jamison.rubino@gmail.com>
2024-02-21 13:37:27 -0800

    minor updates; removed chat bubble, updated booking links, added upwork link, promotional banner

--- jr/src/Components/services.js
+++ jr/src/Components/services.js
@@ -30,7 +30,8 @@ export default function Services() {
     
     const ratingFetch = async () => {
         // scrape https://www.taskrabbit.com/profile/jamison-r--5?ref=TRYBX6ON&utm_medium=tryprofile&invitation_source=url&utm_source=trycode and extract an html element and set rating the html element value
-        const response = await fetch('https://www.taskrabbit.com/profile/jamison-r--5', { method: "GET", headers: {
+        try {
+            const response = await fetch('https://www.taskrabbit.com/profile/jamison-r--5', { method: "GET", headers: {
             "Content-Type": "text/html",
             "Access-Control-Allow-Origin": '*'
         }});
@@ -39,12 +40,10 @@ export default function Services() {
         const doc = parser.parseFromString(data, "text/html");
         const ratingElement = doc.querySelector(".sc-dAbbOL.fSiGRF span");
         setRating(ratingElement.textContent);
-        console.log(ratingElement.textContent);
-        console.log(doc);
-        console.log(data);
-        console.log(response);
-        console.log(rating);
-        
+        console.log(ratingElement.textContent, doc, data, response, rating);
+    } catch(e) {
+        console.log(e);
+    }
 //     fetch('', { method: "GET", // *GET, POST, PUT, DELETE, etc.
     //     headers: {
     //       "Content-Type": "text/html",
@@ -76,6 +75,13 @@ export default function Services() {
     console.log("Services");
     return (
         <div>
+            <div className="row m-0 px-1">
+                <div className="col-12 mx-0 px-2">
+                    <div className="selected-service alert alert-info mx-4 mt-4 mb-0 py-0">
+                        <p className="py-2 m-0 h5">10% OFF IN FEBRUARY</p>
+                    </div>
+                </div>
+            </div>
             <h1
                 className="mt-3 mb-4 pb-2"
                 style={{
@@ -118,9 +124,9 @@ export default function Services() {
                                 }}>Get a Quote</Link>
                             </li>
                             <li className="list-group-item bg-light text-success pb-3">
-                            {/* https://jamison-rubino.bookafy.com/category/moving-help-1803b3?locale=en */}
+                            {/* https://book.housecallpro.com/book/Jamison-Rubino/991a7bc836674660a42c3cfabd4c64e6?v2=true */}
                                 <Link
-                                    to="https://book.housecallpro.com/book/Jamison-Rubino/991a7bc836674660a42c3cfabd4c64e6?v2=true"
+                                    to="https://jamison-rubino.bookafy.com/category/moving-help-1803b3?locale=en"
                                     className="btn btn-dark"
                                     state={{
                                     service: "Moving Help"
@@ -179,9 +185,9 @@ export default function Services() {
                                 }}>Get a Quote</Link>
                             </li>
                             <li className="list-group-item bg-light text-success pb-3">
-                            {/* https://jamison-rubino.bookafy.com/category/furniture-assembly-8d3332?locale=en */}
+                            {/* https://book.housecallpro.com/book/Jamison-Rubino/991a7bc836674660a42c3cfabd4c64e6?v2=true */}
                                 <Link
-                                    to="https://book.housecallpro.com/book/Jamison-Rubino/991a7bc836674660a42c3cfabd4c64e6?v2=true"
+                                    to="https://jamison-rubino.bookafy.com/category/furniture-assembly-8d3332?locale=en"
                                     className="btn btn-dark"
                                     target="_blank">Book Now</Link>
                             </li>
@@ -196,10 +202,23 @@ export default function Services() {
                         <div className="card-body p-4">
                             <h4 className="card-title pb-1 mb-0">Web Development</h4>
                             <h6
-                                className='text-light mb-3'
+                                className='text-light mb-0 pb-1'
                                 style={{
                                 fontSize: "14px"
                             }}>ONLINE</h6>
+                            <h6
+                                className='text-muted mb-3'
+                                style={{
+                                fontSize: "14px"
+                            }}><a href="https://upwork.com/freelancers/~0180302a9d35e1993e" style={{textDecoration: 'none', lineHeight: "18px"}} alt="">
+                            
+                            <div style={{width: "24px", height: "24px", borderRadius: "50%", border: "2px solid #76a4d9", display: "inline-block"}}>
+
+                                <div className="air3-icon md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" aria-hidden="true" viewBox="0 0 24 24" role="img"><path fill="#76a4d9" fill-rule="evenodd" d="M18.37 19.002H5.63v-1.867h12.74v1.867zm.02-3.736H5.608L3 8.314l4.992 1.664L12 5l4.008 4.978L21 8.314l-2.61 6.952z" clip-rule="evenodd"></path></svg></div>
+                            </div>
+                            &nbsp;&nbsp;100% Job Success
+                        </a></h6>
+
 
                             <p className="card-text">Build or improve your personal or business website with
                                 website builders or code.</p>
